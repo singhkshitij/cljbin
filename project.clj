62e7194110d54ac88c6996041973a99ec1e23267
kshitij.singh <kshitij.singh@go-jek.com>
2020-02-16 14:21:55 +0530

    Fixes code

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljbin "0.1.0-SNAPSHOT"
+(defproject cljbin "0.1.0"
   :description "A clojure bin service with syntax validation"
   :url "https://github.com/singhkshitij/cljbin"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -10,6 +10,9 @@
                  [bidi "2.1.3"]
                  [com.taoensso/carmine "2.19.1"]
                  [nano-id "0.10.0"]]
-  :main ^:skip-aot cljbin.core
+  :plugins [[lein-heroku "0.5.3"]]
+  :main cljbin.core
+  :aot [cljbin.core]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :heroku {:app-name "damp-hollows-89882"})
